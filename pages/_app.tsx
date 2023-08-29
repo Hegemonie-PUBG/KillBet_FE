f2f4e0c148e1676be9c6c21d76f4c8c649e80c97
박겸영 <yong_drg@naver.com>
2023-08-29 22:50:41 +0900

    feature(styles): 전역 스타일 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import type { AppProps } from 'next/app';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '@/styles/theme';
 import Layout from './Layout';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   );
 };
 
