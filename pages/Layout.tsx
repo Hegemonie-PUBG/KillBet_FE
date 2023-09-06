5d51faf3a720cb18fec316e72829d2852730e51f
Ahn <tn3748@naver.com>
2023-09-07 02:52:17 +0900

    feat: body layout 스타일 설정

--- pages/Layout.tsx
+++ pages/Layout.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/header';
+import styled from '@emotion/styled';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,9 +9,15 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
-      {children}
+      <Container>{children}</Container>
     </>
   );
 };
 
 export default Layout;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 30px;
+`;
