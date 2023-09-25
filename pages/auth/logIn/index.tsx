bcedab32a3ba9413f3ebcc6b2ffd84626e61d1f0
박겸영 <yong_drg@naver.com>
2023-09-25 22:29:13 +0900

    fix(logIn): lint error 수정

--- pages/auth/logIn/index.tsx
+++ pages/auth/logIn/index.tsx
@@ -1,15 +1,8 @@
 import { Box, Button } from '@mui/material';
 import { RiKakaoTalkFill } from 'react-icons/ri';
 
-import { ROUTE_PATH } from '@/constants/config';
-
 const LogIn = () => {
-  const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-  const REDIRECT_URI = `${
-    typeof window !== 'undefined' && window.location.origin
-  }${ROUTE_PATH.kakaoLogIn}`;
-  // const kakaoUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   const kakaoUrl = `${BASE_URL}/oauth2/authorize/kakao`;
 
   const handleLogIn = () => {
